68894395754b2977359f09a5f49c4b982280d216
Allan Penoni <rochapenoni@gmail.com>
2025-07-05 18:24:24 +0000

    Conclusão do desafio nível novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,8 +7,71 @@
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+
+    int tabuleiro[10][11]; //11 colunas para poder implementar a coluna dos números do tabuleiro.
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 11; j++)
+        {
+            if (j == 0)
+            {
+                tabuleiro[i][j] = i + 1; //Para gerar os números na primeira coluna
+            } 
+            else 
+            {
+                tabuleiro[i][j] = 0; //para gerar as casas com 0
+            }
+            
+        }
+    }
+
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+
+    //Navio horizontal: tabuleiro[3][3], tabuleiro[3][4], tabuleiro[3][5]
+    //Navio vertical: tabuleiro[1][6], tabuleiro[1][7], tabuleiro[1][8]
+    
+    tabuleiro[3][3] = 3; 
+    tabuleiro[3][4] = 3; 
+    tabuleiro[3][5] = 3;
+
+    tabuleiro[5][8] = 3; 
+    tabuleiro[6][8] = 3;
+    tabuleiro[7][8] = 3;
+
+    printf("   A B C D E F G H I J\n"); //para gerar a linha no topo do tabuleiro
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 11; j++)
+        {
+            if (j == 0) //esse if e os ifs aninhados são puramente estéticos, para que todas as linhas e colunas fiquem alinhadas corretamente
+            {
+                if (i == 9)
+                {
+                    printf("%d ", tabuleiro[i][j]);
+                }
+                else
+                {
+                    printf("%d  ", tabuleiro[i][j]);
+                }
+            }
+            else
+            {
+                printf("%d ", tabuleiro[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    
+
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    
+    printf("\nNavio horizontal:\n");
+    printf("C4, D4, E4\n");
+    printf("\n");
+    printf("Navio vertical:\n");
+    printf("H6, H7, H8\n");
+
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
